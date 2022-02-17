cdd53f05600588bc478e0fc18ab2fde425c76bd4
Skuczafiks <99563626+Skuczafiks@users.noreply.github.com>
2022-02-17 22:56:05 +0100

    Add files via upload
    dynamic created elements

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const slider = document.querySelectorAll('.slider-button')
 const statusWindow = document.querySelectorAll('.device-item-screen')
 const closeBtn = document.querySelectorAll('.btn')
 const tempData = document.querySelector('.temp-data')
@@ -8,82 +7,128 @@ const powerData = document.querySelector('.power-data')
 const connectionData = document.querySelectorAll('.connection')
 const statusData = document.querySelectorAll('.status')
 const deviceMenu = document.querySelector('.device-menu')
+const itemsContainer = document.querySelector('.items-container')
 
-let num = 1
-let num2 = 1
-let num3 = 0
-let num4 = 0
+let classIcon = 0
+
+const renderDevices = async () => {
+  let uri = 'http://localhost:3000/api/v1/devices'
+  
+  const res = await fetch(uri)
+  const devices = await res.json()
+  
+  
+  const iconsArr = [ "fas fa-lightbulb", "fas fa-plug", "fas fa-temperature-high", ]
+  
+  let templateDevices = []
+
+  devices.forEach(device => {
+    templateDevices += `
+    <div class="items">
+    <h3>Device type:</h3>
+    <h6>${device.type}</h6>
+    <i class="${iconsArr[classIcon]}"></i>
+    <h6>${device.name}</h6>
+    <div class="slider-button" id=${device.id} onclick="handleBeform(${device.id})"></div>
+    <h6>${device.connectionState}</h6>
+    </div>
+    `
+    console.log(device.type)
+    classIcon ++
+  })
+  itemsContainer.innerHTML = templateDevices
+  if(templateDevices > templateDevices.length) {templateDevices = 0}
+  console.log(templateDevices)
+  
+  let templateWindowScreen = []
+  
+  devices.forEach(device => {
+    const onOff = device.isTurnedOn == true ? "ON" : "OFF"
+    
+    if(device.id == 1) {
+      
+      templateWindowScreen += `
+      <div id=${device.id} class="device-item-screen resizable">
+              <button class="btn" onclick="closeScreen()">x</button>
+              <div class="status-window item">
+                <h4>Brightness:</h4>
+                <h1 class="brightness-data">${device.brightness} %</h1>
+              </div>
+              <div class="item status-window">
+                <h4>Color:</h4>
+                <h1 class="color-data" style="color: ${device.color}">${device.color}</h1>
+              </div>
+              <div class="status-window item">
+              <h4>ON/OFF:</h4>
+              <h1 class="power-data">${onOff}</h1>
+            </div>
+            </div>
+            `
+    } else if (device.id == 2) {
+    
+      templateWindowScreen += `
+      <div id=${device.id} class="device-item-screen resizable">
+              <button id="${device.id}" class="btn" onclick="closeScreen()">x</button>
+              <div class="status-window item" id=${device.id}>
+                <h4>On/Off:</h4>
+                <h1 class="brightness-data">${onOff}</h1>
+              </div>
+              <div class="item status-window" id=${device.id}>
+                <h4>Power Consumtion:</h4>
+                <h1 class="color-data">${device.powerConsumption} Watt</h1>
+              </div>
+            </div>
+            `
+    } else if (device.id == 3) {
+      
+      templateWindowScreen += `
+      <div id=${device.id} class="device-item-screen resizable">
+              <button id="${device.id}" class="btn" onclick="closeScreen()">x</button>
+              <div class="status-window item" id=${device.id}>
+                <h4>Temperature:</h4>
+                <h1 class="brightness-data">${device.temperature} °C</h1>
+              </div>
+            </div>
+            `
+          }
+            
+  })
+  deviceMenu.innerHTML = templateWindowScreen
+}
+
 
 const handleBeform = (e) => {
-  slider.forEach((el) => {
-    const style = el.style
-    el.setAttribute('num', `${num}`)
-    num++
-
-    if (num > slider.length) {
-      num = 1
-    }
-
-    if (el.getAttribute('num') == e.target.getAttribute('num')) {
-      style.setProperty('--left', '60%')
-    } else if (el.getAttribute('num') != e.target.getAttribute('num')) {
-      style.setProperty('--left', '-10%')
+  const slider = document.querySelectorAll('.slider-button')
+  const statusWindow = document.querySelectorAll('.device-item-screen')
+  slider.forEach(el => {
+    if (el.getAttribute('id') == e) {
+      el.style.setProperty('--left', '60%')
+    } else if (el.getAttribute('id') != e) {
+      el.style.setProperty('--left', '-10%')
     }
   })
-
-  if (num2 > slider.length) {
-    num2 = 1
-  }
-
+  
   statusWindow.forEach((el) => {
-    el.setAttribute('num', `${num2}`)
-    num2++
-    if (el.getAttribute('num') == e.target.getAttribute('num')) {
+    if (el.getAttribute('id') == e) {
       el.style.visibility = 'visible'
-    } else if (el.getAttribute('num') != e.target.getAttribute('num')) {
+    } else if (el.getAttribute('id') != e) {
       el.style.visibility = 'hidden'
     }
   })
 }
 
-const downloadData = () => {
-  const URL = ["https://mocki.io/v1/dcbc8fac-8740-4c00-b078-a85691a3d893", "https://mocki.io/v1/587e3460-805e-4ab2-9815-b08467f1b840", "https://mocki.io/v1/3f9a5768-9a2c-412d-877f-c370169fc2d0"] 
-
-  axios.get(URL[Math.floor(Math.random()*URL.length)]).then((res) => {
-    const temp = res.data.devices[2].temperature
-    tempData.textContent = temp + ' °C'
-    const brightness = res.data.devices[0].brightness
-    brightnessData.textContent = brightness + ' %'
-    const color = res.data.devices[0].color
-    colorData.textContent = color
-
-    connectionData.forEach((el) => {
-      let connection = res.data.devices[`${num3}`].connectionState
-      el.setAttribute('num', `${num3}`)
-      num3++
-
-      if (num3 > slider.length - 1) {
-        num3 = 0
-      }
-
-      el.textContent = connection
-    })
-
-    const power = res.data.devices[1].powerConsumption
-    powerData.textContent = power + ' W'
-    console.log(res.data.devices)
+const closeScreen = () => {
+  const slider = document.querySelectorAll('.slider-button')
+  const statusWindow = document.querySelectorAll('.device-item-screen')
+  statusWindow.forEach(el => {
+    el.style.visibility = 'hidden'
+  slider.forEach(el => {
+    el.style.setProperty('--left', '-10%')
+  })  
   })
 }
-
-const closeScreen = () => {
-  statusWindow.forEach((el) => (el.style.visibility = 'hidden'))
-  slider.forEach((el) => el.style.setProperty('--left', '-10%'))
-}
-
-slider.forEach((el) => {
-  el.addEventListener('click', handleBeform)
-})
-
-closeBtn.forEach((el) => el.addEventListener('click', closeScreen))
-
-const myInterval = setInterval(downloadData, 5000)
+  
+  closeBtn.forEach((el) => el.addEventListener('click', closeScreen))
+  
+  // const myInterval = setInterval(downloadData, 5000)
+  window.addEventListener('DOMContentLoaded', () => renderDevices())
